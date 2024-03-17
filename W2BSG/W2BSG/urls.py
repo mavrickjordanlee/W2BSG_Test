31079395a05d1dc57870a8e57c90e076ec62d57f
mavrick-lee <mavrickjordanlee@gmail.com>
2024-03-17 14:36:14 +0800

    Updated database credentials and endpoint urls.

--- W2BSG/W2BSG/urls.py
+++ W2BSG/W2BSG/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from login import views
 
 urlpatterns = [
-    path('register/', views.register_user),
-    path('login/', views.login),
+    path('register', views.register_user),
+    path('login', views.login),
 ]
