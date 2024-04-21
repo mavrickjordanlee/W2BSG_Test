dd36fe5d2abaf8654a324eed0cd30d04aa607566
ZuyiI <100923220+ZuyiI@users.noreply.github.com>
2024-04-21 17:54:46 +0800

    Update urls.py, changed the view function to 'game_details'

--- W2BSG/games/urls.py
+++ W2BSG/games/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('', games_list),
     path('search/<str:game_name>', search_game, name='game-search'),
-    path('<str:_id>', get_game_details, name='game-details'),
+    path('<str:_id>', game_details, name='game-details'),
 ]
